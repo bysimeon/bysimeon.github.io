e2673591af509029743e207e9149e06ac43e4f56
simeon <simeon@u.northwestern.edu>
2018-04-26 17:15:12 -0500

    huh?

--- assets/javascript/start.js
+++ assets/javascript/start.js
@@ -11,6 +11,7 @@ var searchmod = {
   "l:": ["lastfm", "https://www.last.fm/search?q=", "(to right, #d51007, #d32d27)", "#d51007"],
   "g:": ["github", "https://github.com/search?utf8=%E2%9C%93&q=", "(to right, #767676, #999)", "#767676"],
   "u:": ["unsplash", "https://unsplash.com/search/photos/", "(to right, #485563, #29323c)", "#485563"],
+  "n:": ["netflix", "https://www.netflix.com/search?q=", "(to right, #e50914, #e50914)", "#e50914"]
 };
 
 function clear() {
@@ -21,7 +22,7 @@ function clear() {
 }
 
 function cutoff(num, str) {
-  if (str.substring(0,1) != " ") {
+  if (str.substring(0, 1) != " ") {
     num2 = num - 1;
   }
   num2 = num;
@@ -30,67 +31,68 @@ function cutoff(num, str) {
 
 function search(e) {
   var val = document.getElementById("search-field").value;
-  var key = val.substr(0,(cutoff(3, val))).trim().toLowerCase();
+  var key = val.substr(0, (cutoff(3, val))).trim().toLowerCase();
+  console.log(key);
 
   if (val.length <= cutoff(3, val)) {
     if (key in searchmod) {
       searchengine = key;
-      document.getElementById("search-field").style.background ="-webkit-linear-gradient" + searchmod[key][2];
-      document.getElementById("search-field").style.background ="linear-gradient" + searchmod[key][2];
+      document.getElementById("search-field").style.background = "-webkit-linear-gradient" + searchmod[key][2];
+      document.getElementById("search-field").style.background = "linear-gradient" + searchmod[key][2];
       document.getElementById("search-mode").style.color = searchmod[key][3];
       document.getElementById("search-mode").style.color = searchmod[key][3];
       document.getElementById("search-mode").innerHTML = searchmod[key][0];
     }
   }
 
-  if (! val.includes(":") && ! val.includes("/")) {
+  if (!val.includes(":") && !val.includes("/")) {
     clear();
   }
 
   if (e.keyCode == 13) {
     if (searchengine != "") {
-      window.open((searchmod[key][1])+ val.substr(cutoff(3, val)).trim(), "_self");
-    }
-    else if (val.includes('.com') || val.includes('.net') || val.includes('.co') ||
-    val.includes('.io') || val.includes('.xyz') || val.includes('.gov') || val.includes('.org') || val.includes('.se') ||
-    val.includes('.fm') || val.includes('.de') || val.includes('.uk') || val.includes('.gg') || val.includes('.info') ||
-    val.includes('.ai') || val.includes('.ch') || val.includes('.edu') || val.includes('.gl') ||  val.includes('.nz')) {
+      window.open((searchmod[key][1]) + val.substr(cutoff(3, val)).trim(), "_self");
+    } else if (val.includes('.com') || val.includes('.net') || val.includes('.co') ||
+      val.includes('.io') || val.includes('.xyz') || val.includes('.gov') || val.includes('.org') || val.includes('.se') ||
+      val.includes('.fm') || val.includes('.de') || val.includes('.uk') || val.includes('.gg') || val.includes('.info') ||
+      val.includes('.ai') || val.includes('.ch') || val.includes('.edu') || val.includes('.gl') || val.includes('.nz') || val.includes('.so')) {
       if (val.includes('http')) {
         window.open(val.trim(), "_self");
       } else {
         window.open("http://" + val.trim(), "_self");
       }
-    }
-    else {
-     window.open("https://google.com/search?q=" + val.trim(), "_self");
+    } else {
+      window.open("https://google.com/search?q=" + val.trim(), "_self");
     }
   }
 }
 
 const monthNames = ["january", "february", "march", "april", "may", "june",
-"july", "august", "september", "october", "november", "december" ];
+  "july", "august", "september", "october", "november", "december"
+];
 
 const dayNames = ["", "first", "second", "third", "fourth", "fifth", "sixth",
-"seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth", "thirteenth", "fourteenth",
-"fiftheenth", "sixteenth", "seventeenth", "eighteenth", "nineteenth", "twentyith", "twentyfirst",
-"twentysecond", "twentythird", "twentyfourth", "twentyfifth", "twentysixth", "twenyseventh", "twentyeight",
-"twentyninth", "thirtyith", "thirtyfirst"];
+  "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth", "thirteenth", "fourteenth",
+  "fiftheenth", "sixteenth", "seventeenth", "eighteenth", "nineteenth", "twentyith", "twentyfirst",
+  "twentysecond", "twentythird", "twentyfourth", "twentyfifth", "twentysixth", "twenyseventh", "twentyeight",
+  "twentyninth", "thirtyith", "thirtyfirst"
+];
 
 function getTime() {
   let date = new Date(),
-  min = date.getMinutes(),
-  sec = date.getSeconds(),
-  hour = date.getHours();
+    min = date.getMinutes(),
+    sec = date.getSeconds(),
+    hour = date.getHours();
 
   return "" +
-  (hour < 10 ? ("0" + hour) : hour) + ":" +
-  (min < 10 ? ("0" + min) : min) + ":" +
-  (sec < 10 ? ("0" + sec) : sec);
+    (hour < 10 ? ("0" + hour) : hour) + ":" +
+    (min < 10 ? ("0" + min) : min) + ":" +
+    (sec < 10 ? ("0" + sec) : sec);
 }
 
 function getDay() {
   let date = new Date(),
-  day = dayNames[date.getDate()];
+    day = dayNames[date.getDate()];
   month = monthNames[date.getMonth()];
 
   return "" + month + " " + day;
@@ -124,7 +126,7 @@ window.onload = () => {
 
   function recenttrack() {
     let xhr2 = new XMLHttpRequest();
-    xhr2.open('GET', apibase + "?method=user.getrecenttracks&user=" + user +  "&api_key=" + apikey + "&format=json");
+    xhr2.open('GET', apibase + "?method=user.getrecenttracks&user=" + user + "&api_key=" + apikey + "&format=json");
     xhr2.onload = () => {
       if (xhr2.readyState === 4) {
         if (xhr2.status === 200) {
@@ -139,17 +141,15 @@ window.onload = () => {
           }
           if (songname.length >= 50) {
             if (songname.includes('(f')) {
-              songname = (songname.substr(0,songname.indexOf(' (f')));
-            }
-            else {
-              songname = (songname.substr(0,50)) + "...";
+              songname = (songname.substr(0, songname.indexOf(' (f')));
+            } else {
+              songname = (songname.substr(0, 50)) + "...";
             }
           }
           if (playing) {
-            document.getElementById('song').innerHTML = songname  + " - " + artistname;
+            document.getElementById('song').innerHTML = songname + " - " + artistname;
             document.getElementById('song').setAttribute("href", json2.recenttracks.track[0].url);
-          }
-          else {
+          } else {
             document.getElementById('song').innerHTML = noscrobble;
             document.getElementById('song').setAttribute("href", "https://www.last.fm/user/theblindlookout");
           }
@@ -170,16 +170,16 @@ window.onload = () => {
   document.getElementById("clock").innerHTML = getTime();
   document.getElementById("date").innerHTML = getDay();
 
-  setInterval( () => {
+  setInterval(() => {
     document.getElementById("clock").innerHTML = getTime();
     document.getElementById("date").innerHTML = getDay();
-  },100);
+  }, 100);
 }
 
 document.addEventListener("keydown", event => {
-  if (event.keyCode == 32) {          // Spacebar code to open search
+  if (event.keyCode == 32) { // Spacebar code to open search
     opensearchbox();
-  } else if (event.keyCode == 27) {   // Esc to close search
+  } else if (event.keyCode == 27) { // Esc to close search
     closeall();
   }
 });
