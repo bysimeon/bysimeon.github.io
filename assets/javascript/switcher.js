32ad2801f649bc4e3d0570be16b9b359a3f64884
Simeon <simeon@u.northwestern.edu>
2018-10-31 17:34:33 -0500

    fix fixes

--- assets/javascript/switcher.js
+++ assets/javascript/switcher.js
@@ -1,18 +1,33 @@
-var colors = [["#EEEEEE", "#212121"], [("#fff", "#000")], ["#000", "#fff"]];
+var colors = [
+  ["#EEEEEE", "#212121"], 
+  ["#fff", "#000"], 
+  ["#000", "#fff"]];
 
 var curr = 0;
-var curr = localStorage.getItem('curr');
+var curr = localStorage.getItem("curr");
 
 function darkMode() {
-  document.documentElement.style.setProperty('--text-color', colors[curr%colors.length][0]);
-  document.documentElement.style.setProperty('--background-color', colors[curr%colors.length][1]);
-  localStorage.setItem('textColor', colors[curr%colors.length][0]);
-  localStorage.setItem('backgroundColor', colors[curr%colors.length][1]);
+  document.documentElement.style.setProperty(
+    "--text-color",
+    colors[curr % colors.length][0]
+  );
+  document.documentElement.style.setProperty(
+    "--background-color",
+    colors[curr % colors.length][1]
+  );
+  localStorage.setItem("textColor", colors[curr % colors.length][0]);
+  localStorage.setItem("backgroundColor", colors[curr % colors.length][1]);
   curr++;
-  localStorage.setItem('curr', curr);
+  localStorage.setItem("curr", curr);
 }
 
 window.onload = () => {
-  document.documentElement.style.setProperty('--text-color', localStorage.getItem('textColor'));
-  document.documentElement.style.setProperty("--background-color", localStorage.getItem("backgroundColor"));
-}
+  document.documentElement.style.setProperty(
+    "--text-color",
+    localStorage.getItem("textColor")
+  );
+  document.documentElement.style.setProperty(
+    "--background-color",
+    localStorage.getItem("backgroundColor")
+  );
+};
