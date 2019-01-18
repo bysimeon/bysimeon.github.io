3b2d2e662ff2a9b5d96399e66323fa6a49cc8db8
Simeon <simeon@u.northwestern.edu>
2019-01-17 18:01:32 -0600

    lintgang [eslint + prettier]

--- assets/javascript/time.js
+++ assets/javascript/time.js
@@ -1,40 +1,83 @@
-const monthNames = ["january", "february", "march", "april", "may", "june",
-  "july", "august", "september", "october", "november", "december"
-];
+const monthNames = [
+    "january",
+    "february",
+    "march",
+    "april",
+    "may",
+    "june",
+    "july",
+    "august",
+    "september",
+    "october",
+    "november",
+    "december"
+]
 
-const dayNames = ["", "first", "second", "third", "fourth", "fifth", "sixth",
-  "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth", "thirteenth", "fourteenth",
-  "fiftheenth", "sixteenth", "seventeenth", "eighteenth", "nineteenth", "twentyith", "twentyfirst",
-  "twenysecond", "twentythird", "twenyfourth", "twenyfifth", "twenysixth", "twenysevenph", "twenyayth",
-  "twenyninth", " thirtyith", "thirtyfirst"
-];
+const dayNames = [
+    "",
+    "first",
+    "second",
+    "third",
+    "fourth",
+    "fifth",
+    "sixth",
+    "seventh",
+    "eighth",
+    "ninth",
+    "tenth",
+    "eleventh",
+    "twelfth",
+    "thirteenth",
+    "fourteenth",
+    "fiftheenth",
+    "sixteenth",
+    "seventeenth",
+    "eighteenth",
+    "nineteenth",
+    "twentyith",
+    "twentyfirst",
+    "twenysecond",
+    "twentythird",
+    "twenyfourth",
+    "twenyfifth",
+    "twenysixth",
+    "twenysevenph",
+    "twenyayth",
+    "twenyninth",
+    " thirtyith",
+    "thirtyfirst"
+]
 
 function getTime() {
-  let date = new Date(),
-    min = date.getMinutes(),
-    sec = date.getSeconds(),
-    hour = date.getHours();
+    let date = new Date(),
+        min = date.getMinutes(),
+        sec = date.getSeconds(),
+        hour = date.getHours()
 
-  return "" +
-    (hour < 10 ? ("0" + hour) : hour) + ":" +
-    (min < 10 ? ("0" + min) : min) + ":" +
-    (sec < 10 ? ("0" + sec) : sec);
+    return (
+        "" +
+        (hour < 10 ? "0" + hour : hour) +
+        ":" +
+        (min < 10 ? "0" + min : min) +
+        ":" +
+        (sec < 10 ? "0" + sec : sec)
+    )
 }
 
 function getDay() {
-  let date = new Date(),
-    day = dayNames[date.getDate()];
-  month = monthNames[date.getMonth()];
+    let date = new Date(),
+        day = dayNames[date.getDate()]
+    month = monthNames[date.getMonth()]
 
-  return "" + month + " " + day;
+    return "" + month + " " + day
 }
 
 window.onload = () => {
-  document.getElementById("clock").innerHTML = getTime();
-  document.getElementById("date").innerHTML = getDay();
+    document.getElementById("clock").innerHTML = getTime()
+    document.getElementById("date").innerHTML = getDay()
 
-  setInterval(() => {
-    document.getElementById("clock").innerHTML = getTime();
-    document.getElementById("date").innerHTML = getDay();
-  }, 100);
+    setInterval(() => {
+        document.getElementById("clock").innerHTML = getTime()
+        document.getElementById("date").innerHTML = getDay()
+    }, 100)
 }
