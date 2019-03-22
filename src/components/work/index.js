9411df34385a359d20dbe7d5fc9d86435e7fb082
Simeon <simeon@u.northwestern.edu>
2019-03-21 20:20:47 -0400

    updated work

--- src/components/work/index.js
+++ src/components/work/index.js
@@ -16,63 +16,66 @@ class Work extends Component {
                     </span> some <span className="paid filter">paid</span> some{" "}
                     <span className="unfinished filter">unfinished</span>
                 </h1>
-                <ul className="projects">
-                    <li className="paid">
-                        the highly [w/{" "}
-                        <a href="https://teahkbrands.com"> teah</a>] |{" "}
+                <div className="projects">
+                    <div className="project paid">
+                        <b>the highly</b> with{" "}
+                        <a href="https://teahkbrands.com"> teah</a> |{" "}
                         <a href="https://www.thehighly.co">live</a>{" "}
                         <a href="https://web.archive.org/web/20181203171025/https://www.thehighly.co/">
                             archive
                         </a>
-                    </li>
-                    <li className="personal">
-                        startpage | <a href="/start/">live</a>{" "}
+                    </div>
+                    <div className="personal project">
+                        <b>startpage</b> | <a href="/start/">live</a>{" "}
                         <a href="https://github.com/bysimeon/startpage">repo</a>
-                    </li>
-                    <li className="paid">
-                        storymapjs [w/{" "}
+                    </div>
+                    <div className="paid project">
+                        <b>storymapjs</b> with{" "}
                         <a href="https://knightlab.northwestern.edu/">
                             knight lab
-                        </a>
-                        ] | <a href="https://storymap.knightlab.com/">live</a>{" "}
+                        </a>{" "}
+                        | <a href="https://storymap.knightlab.com/">live</a>{" "}
                         <a href="https://github.com/NUKnightLab/StoryMapJS">
                             repo
                         </a>
-                    </li>
-                    <li className="personal">
-                        vista del valle [w/{" "}
-                        <a href="https://teahkbrands.com/">teah</a>] |{" "}
+                    </div>
+                    <div className="personal project">
+                        <b>vista del valle</b> with{" "}
+                        <a href="https://teahkbrands.com/">teah</a> |{" "}
                         <a href="https://vistadv.github.io/">live</a>{" "}
-                    </li>
-                    <li className="personal">
-                        the grapplers [w/{" "}
+                    </div>
+                    <div className="personal project">
+                        <b>the grapplers</b> with{" "}
                         <a href="https://github.com/AndrewBridgeman">andrew</a>
                         {" & "}
-                        <a href="http://zacharyrmckee.com/home/">zach</a>] |{" "}
+                        <a href="http://zacharyrmckee.com/home/">zach</a> |{" "}
                         <a href="https://github.com/bysimeon/tentativetitle/releases">
                             play
                         </a>{" "}
                         <a href="https://github.com/bysimeon/tentativetitle/">
                             repo
                         </a>{" "}
-                    </li>
-                    <li className="unfinished paid">
-                        tiilt lab [w/{" "}
-                        <a href="https://github.com/durellg">durell</a>] |{" "}
+                    </div>
+                    <div className="unfinished paid project">
+                        <b>tiilt lab website</b> with{" "}
+                        <a href="https://github.com/durellg">durell</a> |{" "}
                         <a href="http://tiilt.northwestern.edu/">
                             workinprogress
                         </a>
-                    </li>
+                    </div>
 
-                    <li className="personal">
-                        papi [w/ <a href="https://github.com/deokfilho">deok</a>
+                    <div className="personal project">
+                        <b>papi</b> with{" "}
+                        <a href="https://github.com/deokfilho">deok</a>
                         {" & "}
-                        <a href="https://github.com/addierohrbach">addie</a>] |{" "}
+                        <a href="https://github.com/addierohrbach">
+                            addie
+                        </a> |{" "}
                         <a href="https://papi-law.herokuapp.com/">
                             workinprogress
                         </a>{" "}
-                    </li>
-                </ul>
+                    </div>
+                </div>
             </div>
         )
     }
