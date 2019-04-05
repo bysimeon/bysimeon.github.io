485c3361ac191fa9a6a6d1534f21784f78153632
Simeon <simeon@u.northwestern.edu>
2019-04-04 22:00:37 -0500

    new images , reversed order

--- src/components/else/images/index.js
+++ src/components/else/images/index.js
@@ -18,7 +18,10 @@ class Images extends Component {
     createImages() {
         let images = []
         for (let i = 0; i < this.state.totalImageCount; i++) {
-            images.push({ url: this.state.path + "image" + i + ".jpg", id: i })
+            images.unshift({
+                url: this.state.path + "image" + i + ".jpg",
+                id: this.state.totalImageCount - i - 1
+            })
         }
         this.setState({
             images: images
@@ -34,6 +37,7 @@ class Images extends Component {
     loadPreview(e) {
         e.stopPropagation()
         let index = e.target.getAttribute("id")
+        console.log(parseInt(index))
         this.setState({
             preview: true,
             previewIndex: parseInt(index)
@@ -70,12 +74,15 @@ class Images extends Component {
     }
 
     componentDidMount() {
-        this.setState({
-            path: this.props.path,
-            totalImageCount: this.props.count
-        },() => {
-        this.createImages()
-        })
+        this.setState(
+            {
+                path: this.props.path,
+                totalImageCount: this.props.count
+            },
+            () => {
+                this.createImages()
+            }
+        )
     }
 
     render() {
@@ -95,10 +102,13 @@ class Images extends Component {
             <div>
                 <div className="images">
                     <p className="description">
-                        {this.state.loadedImageCount >= this.state.totalImageCount ? (
+                        {this.state.loadedImageCount >=
+                        this.state.totalImageCount ? (
                             <span>{this.props.tag} photos taken bysimeon</span>
                         ) : (
-                            <span>loading {this.props.tag} photos taken bysimeon</span>
+                            <span>
+                                loading {this.props.tag} photos taken bysimeon
+                            </span>
                         )}
                     </p>
                     {images}
@@ -126,11 +136,11 @@ class Images extends Component {
                         >
                             <svg className="images__navigation images__navigation--right">
                                 <path
-                                    class="shape-outline outer-color"
+                                    className="shape-outline outer-color"
                                     d="M8.0008001,17.2999992c-0.4000001,0.3999996-0.4000001,1,0,1.3999996l15.7991991,15.7992001								c0.3999996,0.4000015,1,0.4000015,1.3999996,0l2.6992016-2.6991997c0.3999996-0.3999996,0.3999996-1,0-1.3999996								L16.2992001,18.7000008c-0.4000006-0.3999996-0.4000006-1,0-1.3999996L27.8992004,5.6999998								c0.3999996-0.4000001,0.3999996-1,0-1.4000001l-2.6992016-2.6992002c-0.3999996-0.4-1-0.4-1.3999996,0L8.0008001,17.2999992z"
                                 />
                                 <path
-                                    class="shape-shape inner-color"
+                                    className="shape-shape inner-color"
                                     d="M9.6999998,17.6000004c-0.1999998,0.2000008-0.1999998,0.5,0,0.7000008l14.500001,14.5000019								c0.2000008,0.2000008,0.5,0.2000008,0.7000008,0l1.3999996-1.3999996c0.2000008-0.2000008,0.2000008-0.5,0-0.7000008								L13.8999996,18.3999996c-0.1999998-0.2000008-0.1999998-0.5,0-0.7000008L26.2999992,5.3000002								c0.2000008-0.1999998,0.2000008-0.5,0-0.6999998l-1.3999996-1.4000001c-0.2000008-0.2-0.5-0.2-0.7000008,0L9.6999998,17.6000004z"
                                 />
                             </svg>
